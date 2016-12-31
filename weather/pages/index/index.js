8ae3f164096a477cde84f75b948fe6d939e236fa
zhangh <740586220@qq.com>
2016-12-31 15:49:09 +0800

    map

--- weather/pages/index/index.js
+++ weather/pages/index/index.js
@@ -16,9 +16,23 @@ Page({
   },
   location: function() {
     var that = this;
+    console.log(this.data.location.latitude,this.data.location.longitude)
     wx.openLocation({
       latitude: that.data.location.latitude, // 纬度，范围为-90~90，负数表示南纬
       longitude: that.data.location.longitude // 经度，范围为-180~180，负数表示西经
+    })
+  },
+  openmap:function() {
+    wx.chooseLocation({
+      success: function(res){
+        console.log(res)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
     })
   },
   onLoad: function () {
@@ -32,8 +46,9 @@ Page({
       })
     })
     wx.getLocation({
-      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
       success: function(res){
+        console.log(res)
         that.setData({
           location:{
            latitude:res.latitude,
