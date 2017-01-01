38aab07df149a4bc61e7631710cf15fa40796b79
zhangh <740586220@qq.com>
2017-01-02 01:41:04 +0800

    commit

--- weather/utils/getWeatherByLocation.js
+++ weather/utils/getWeatherByLocation.js
@@ -1,4 +1,5 @@
-//使用的是和风天气 
+//使用的是和风天气   但是我得研究一下如何只提供经纬度就能获得信息 
+//这个api只要中英文名代码和ip
 
 let getWeatherByLocation = (latitude, longtitude, callback) => {
     var apiKey = 'de0b097f055743c79af50e4836adabdc'
